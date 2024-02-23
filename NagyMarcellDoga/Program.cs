00c98b07fb2e165d0245ead4c3da27676b138c62
marci1200 <nagymarci.0218@gmail.com>
2024-02-23 11:55:18 +0100

    Add project files.

---
+++ NagyMarcellDoga/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace NagyMarcellDoga
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var taborok = new List<Tabor>();
+            using (var sr = new StreamReader(@"taborok.txt"))
+            {
+                while (!sr.EndOfStream) taborok.Add(new Tabor(sr.ReadLine()));
+            }
+
+            Console.WriteLine("2. feladat:");
+            Console.WriteLine($"Az adatsorok száma: {taborok.Count()}");
+            Console.WriteLine($"Az első rögzített téma: {taborok[0].Tema}");
+            Console.WriteLine($"Az utolsó rögzített téma: {taborok[taborok.Count - 1].Tema}");
+
+            Console.WriteLine("3. feladat:");
+            bool van = false;
+            for (int i = 0; i < taborok.Count; i++)
+            {
+                if (taborok[i].Tema == "zenei")
+                {
+                    van = true;
+                    Console.WriteLine($"Zenei tábor kezdődik {taborok[i].KezdHo}. hó {taborok[i].KezdNap}. napján!");
+                }
+
+            }
+
+            if (!van)
+            {
+                Console.WriteLine("Nincsen zenei tábor!");
+            }
+
+            Console.WriteLine("4. feladat");
+            int kezdh = 0, kezdn = 0;
+            int max = 10;
+            string tema = "";
+            foreach (var tabor in taborok)
+            {
+                if (max < tabor.Kezdobetuk.Length)
+                {
+                    kezdh = tabor.KezdHo;
+                    kezdn = tabor.KezdNap;
+                    tema = tabor.Tema;
+                }
+            }
+
+            Console.WriteLine("Legnépszerűbbek:");
+            Console.WriteLine(kezdh + " " + kezdn + " " + tema);
+
+
+            Console.WriteLine("6. feladat:");
+            Console.Write("Hó: ");
+            int ho = int.Parse(Console.ReadLine());
+            Console.Write("Nap: ");
+            int nap = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            ////foreach (var tabor in taborok)
+            ////{
+            ////    if(sorszam(ho, nap))
+            ////    {
+            ////        count++;
+            ////    }
+            ////}
+
+            Console.WriteLine($"Ekkor éppen {count} tábor zajlik");
+
+            Console.ReadKey();
+        }
+
+        public int sorszam(int honap, int nap)
+        {
+            DateTime kezdes = new DateTime(2024, 6, 16);
+            DateTime most = new DateTime(2024, honap, nap);
+
+            TimeSpan kapott = most - kezdes;
+
+            return kapott.Days;
+        }
+    }
+}
---
+++ NagyMarcellDoga/Tabor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NagyMarcellDoga
+{
+    internal class Tabor
+    {
+        public int KezdHo { get; set; }
+        public int KezdNap { get; set; }
+        public int VegHo { get; set; }
+        public int VegNap { get; set; }
+        public string Kezdobetuk { get; set; }
+        public string Tema { get; set; }
+
+        public Tabor(string v)
+        {
+            string[] adatok = v.Split('\t');
+            KezdHo = int.Parse(adatok[0]);
+            KezdNap = int.Parse(adatok[1]);
+            VegHo = int.Parse(adatok[2]);
+            VegNap = int.Parse(adatok[3]);
+            Kezdobetuk = adatok[4];
+            Tema = adatok[5];
+        }
+    }
+}
